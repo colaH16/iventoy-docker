effdbd11a923aaae59ee92b073b991366c36acea
colaH16 <colah16@outlook.com>
2023-12-20 22:54:37 +0900

    add docker push

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/zsh
 
-TAG=cola16/iventoy:debian-bookworm-slim-2023-1220-1
+TAG=cola16/iventoy-debian:debian-bookworm-slim-2023-1220-2
 IVENTOY_VERSION=1.0.19
 docker build --rm --build-arg="IVENTOY_VERSION_NS=$IVENTOY_VERSION" --tag=$TAG .
+docker push $TAG
